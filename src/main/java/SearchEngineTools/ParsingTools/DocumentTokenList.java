872c4ca8a9dd16f71587b7b0b5020a8882018b3b
benarili <benarili@post.bgu.ac.il>
2018-12-05 12:34:24 +0200

    less instances of invoking api

--- src/main/java/SearchEngineTools/ParsingTools/DocumentTokenList.java
+++ src/main/java/SearchEngineTools/ParsingTools/DocumentTokenList.java
@@ -3,7 +3,6 @@ package SearchEngineTools.ParsingTools;
 import SearchEngineTools.ParsingTools.Term.CityTerm;
 import eu.fayder.restcountries.v1.domain.Country;
 import eu.fayder.restcountries.v1.rest.CountryService;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -143,35 +142,45 @@ public class DocumentTokenList implements ITokenList {
      * @param currentLine
      */
     private void extractCityTerm(String currentLine) {
-        String lineWithoutTag = currentLine.length()>=10 ? currentLine.substring(9) : null;
-        if(lineWithoutTag==null)
-            return;
-        String[] splitLineWithoutTag = lineWithoutTag.split(" ");
-        String cityName="";
+        String cityName = null;
         boolean foundCity = false;
-        String lastCityName = null;
-        Country lastCountry = null;
-        int usedAPI = 0;
-        for (int i = 0; i < splitLineWithoutTag.length & usedAPI<3; i++) {
-            String currentIndexString = splitLineWithoutTag[i];
-            if(currentIndexString!=null && currentIndexString.length()>0){
-                cityName += currentIndexString;
-                List<Country> country = countryService.getByCapital(cityName);
-                usedAPI++;
-                if(country!=null && !country.isEmpty()) {
-                    lastCityName=cityName;
-                    lastCountry=country.get(0);
-                    foundCity=true;
+        Country country = null;
+        List<String> cityNameWords = getLongestCityNameCandidate(currentLine,2);
+        if(cityNameWords == null)
+            return;
+        for (int i = cityNameWords.size()-1; i >= 0 && !foundCity; i--) {
+            cityName = "";
+            for (int j = 0; j <= i; j++) {
+                if(j!=0)
                     cityName+=" ";
-                }
-                else {
-                    cityName+=" ";
-                }
+                cityName+=cityNameWords.get(j);
+            }
+            List<Country> countryCandidate = countryService.getByCapital(cityName);
+            if(countryCandidate!=null && !countryCandidate.isEmpty()){
+                country = countryCandidate.get(0);
+                foundCity = true;
             }
         }
         if(foundCity){
-            cityTerm = new CityTerm(lastCityName,lastCountry);
+            cityTerm = new CityTerm(cityName,country);
         }
+    }
+
+    private List<String> getLongestCityNameCandidate(String cityLine, int maxWordsInName){
+        String lineWithoutTag = currentLine.length()>=10 ? currentLine.substring(9) : null;
+        if(lineWithoutTag==null)
+            return null;
+        String[] splitLineWithoutTag = lineWithoutTag.split(" ");
+        List<String> cityName = new ArrayList<>(maxWordsInName);
+        int words = 0;
+        for (int i = 0; i < splitLineWithoutTag.length && words<maxWordsInName; i++) {
+            String currentIndexString = splitLineWithoutTag[i];
+            if(currentIndexString!=null && currentIndexString.length()!=0 && !currentIndexString.equals("</F>")){
+                cityName.add(currentIndexString);
+                words++;
+            }
+        }
+        return cityName;
     }
 
     @Override
